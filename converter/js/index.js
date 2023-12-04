771299843802d4522df68cba4a0cbb350cebfaf0
Andrey <andreeey.gg@gmail.com>
2023-12-04 22:29:45 +0100

    initial commit html + css + js (add codes)

---
+++ converter/js/index.js
@@ -0,0 +1,37 @@
+import variables from './variables.js'
+import state from './state.js'
+
+
+
+const { selects, success } = variables
+
+
+const handleChange = ({target: {value}}) => {
+    console.log(value)
+}
+const renderCodeList = () => {
+    selects.forEach((select) => {
+        state.codes.forEach(([code]) => {
+            const element = document.createElement('option')
+            element.value = code
+            element.innerText = code
+            select.insertAdjacentElement('beforeend', element)
+        })
+        select.addEventListener('change', handleChange)
+    })
+}
+const fetchCodes = async() => {
+    try {
+        const response = await fetch(`${state.url}/codes`)
+        const data = await response.json()
+        if (data.result === success ) {
+            state.codes = data.supported_codes
+            renderCodeList()
+        }
+
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+fetchCodes()
---
+++ converter/js/state.js
@@ -0,0 +1,5 @@
+
+export default {
+    url: `https://v6.exchangerate-api.com/v6/39318d689c7c2a578c60e570/`,
+    codes: [],
+}
