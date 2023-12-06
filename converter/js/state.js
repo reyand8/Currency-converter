d8e9ee5fb8770b48ab5ff036c5cb51252c73bc5b
Andrey <andreeey.gg@gmail.com>
2023-12-06 18:29:36 +0100

    add state

--- converter/js/state.js
+++ converter/js/state.js
@@ -2,4 +2,19 @@
 export default {
     url: `https://v6.exchangerate-api.com/v6/39318d689c7c2a578c60e570/`,
     codes: [],
+    pair: {
+        from: '',
+        to: '',
+    },
+    amount: '',
+    loading: false,
+    currentTab: "convert",
+    currency: {
+        code: "USD",
+    },
+    currencies: ["USD", "EUR", "UAH"],
+    actions: {
+        remove: 'remove',
+        change: 'change',
+    },
 }
