5847b17b104b53e03edd5712de4fd031a28a0126
Andrey <andreeey.gg@gmail.com>
2023-12-07 18:39:30 +0100

    add js

---
+++ converter/js/events.js
@@ -0,0 +1,21 @@
+import { handleInputAmount, handleSubmit, switchCurrencies } from "./convert.js";
+import { fetchCodes, handleTabClick } from "./index.js";
+import { addCurrency, handleActionClick, handleAddSelectChange, handleSingleSelectChange,
+} from "./single.js";
+
+import variables from "./variables.js";
+
+const { tabs, form, amountInput, switchButton,
+    currentCurrency, currentCurrencyList, singleSelect, addButton, addCurrencySelect}
+    = variables;
+
+fetchCodes();
+amountInput.addEventListener("keyup", handleInputAmount)
+form.addEventListener("submit", handleSubmit)
+switchButton.addEventListener("click", switchCurrencies);
+tabs.forEach((tab) => tab.addEventListener("click", handleTabClick))
+currentCurrency.addEventListener("click", handleActionClick)
+currentCurrencyList.addEventListener("click", handleActionClick)
+singleSelect.addEventListener("change", handleSingleSelectChange)
+addButton.addEventListener("click", addCurrency)
+addCurrencySelect.addEventListener("change", handleAddSelectChange)
--- converter/js/index.js
+++ converter/js/index.js
@@ -1,37 +1,50 @@
-import variables from './variables.js'
-import state from './state.js'
+import variables from "./variables.js";
+import state from "./state.js";
+import { handleChangeCode } from "./convert.js";
+import { fetchLatest } from "./single.js";
 
+const { currencySelects, success, tabs } = variables
 
-
-const { selects, success } = variables
-
-
-const handleChange = ({target: {value}}) => {
-    console.log(value)
-}
 const renderCodeList = () => {
-    selects.forEach((select) => {
+    currencySelects.forEach((select) => {
         state.codes.forEach(([code]) => {
             const element = document.createElement('option')
             element.value = code
             element.innerText = code
             select.insertAdjacentElement('beforeend', element)
         })
-        select.addEventListener('change', handleChange)
+        const name = select.getAttribute('name')
+        name && select.addEventListener('change', handleChangeCode)
     })
 }
-const fetchCodes = async() => {
+
+export const fetchCodes = async () => {
     try {
         const response = await fetch(`${state.url}/codes`)
         const data = await response.json()
-        if (data.result === success ) {
+        if (data.result === success) {
             state.codes = data.supported_codes
             renderCodeList()
+            fetchLatest()
         }
-
-    } catch(err) {
+    } catch (err) {
         console.log(err)
     }
 }
 
-fetchCodes()
+export const handleTabClick = ({ currentTarget: target }) => {
+    const { tab } = target.dataset
+    const children = document.querySelectorAll('.content')
+    if (!tab || tab === state.currentTab)
+        return
+    tabs.forEach((item) => item.classList.remove('active'))
+    target.classList.add('active')
+    for (const child of children) {
+        if (child.dataset.child === tab) {
+            child.classList.add('show')
+        } else {
+            child.classList.remove('show')
+        }
+    }
+    state.currentTab = tab
+}
