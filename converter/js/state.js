91cc965fa9a2dd1f01c7fe6a2e5513b52f12cc72
Andrey <andreeey.gg@gmail.com>
2023-12-10 18:52:15 +0100

    change api

--- converter/js/state.js
+++ converter/js/state.js
@@ -1,6 +1,6 @@
 
 export default {
-    url: `https://v6.exchangerate-api.com/v6/39318d689c7c2a578c60e570/`,
+    url: `https://v6.exchangerate-api.com/v6/{API KEY}/`,
     codes: [],
     pair: {
         from: '',
